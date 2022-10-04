a4906e73293658ba240450e013ef655662657ee7
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-10-04 09:40:27 +0300

    minor edits to pass tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :campers, only: [:index, :show, :create]
-  resources :signups
+  resources :signups, only: [:create]
   resources :activities, only: [:index, :destroy]
 end
